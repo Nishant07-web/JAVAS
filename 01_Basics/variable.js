813e49d9dc03ab75e5e789e0925a715b5ed4975a
Kumar Nishant <128643933+Nishant07-web@users.noreply.github.com>
2023-10-10 18:23:08 +0000

    variable

---
+++ 01_Basics/variable.js
@@ -0,0 +1,11 @@
+const accountId=456234
+let accountemail="Nishant26346@gmail.com"
+var accountpassword="1234"
+accountcity="Jharkhand"
+console.log(accountId);
+ accountemail="Bittu26346@gmail.com"
+ accountpassword="4567"
+accountcity="Uttarpradesh"
+console.log(accountemail);
+console.log(accountpassword);
+console.log(accountcity);
